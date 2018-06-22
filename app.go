82079a933cfb8d8fab6cdb4a52e7887e32df672b
Dan <tdangarfield@gmail.com>
2018-06-22 14:49:10 -0700

    Add death by click and new routes

--- app.go
+++ app.go
@@ -13,8 +13,20 @@ func main() {
 	} 
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<html style='background:" + os.Getenv("COLOR") + "'> Requested: %s\n </html>", r.URL.Path)
+		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:" + os.Getenv("COLOR") + "'> Requested: %s\n </html>", r.URL.Path)
+	})
+
+	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html> DASHBOARD Requested: %s\n </html>", r.URL.Path)
+	})
+
+	http.HandleFunc("/die", func(w http.ResponseWriter, r *http.Request) {
+		die();
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func die() {
+	os.Exit(3)
 }
