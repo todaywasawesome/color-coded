76bd30644f53acc31508beb8f60280572eaad9fa
Dan <tdangarfield@gmail.com>
2018-10-25 18:14:40 -0700

    Reset everything before meetup

--- app.go
+++ app.go
@@ -8,11 +8,11 @@ import (
 )
  
 func main() {
-	time.Sleep(10 * time.Second)
-	die();
+	time.Sleep(1 * time.Second)
+	#die();
 	c := os.Getenv("COLOR")
 	if len(c) == 0{
-		os.Setenv("COLOR", "#F1A94E") //Blue 44B3C2 and Yellow F1A94E. 
+		os.Setenv("COLOR", "#44B3C2") //Blue 44B3C2 and Yellow F1A94E. 
 	} 
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
