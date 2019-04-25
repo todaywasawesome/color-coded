13d4bc364827f64860122c7193412e6c48bdb901
Dan <tdangarfield@gmail.com>
2019-04-25 09:03:58 -0600

    Add security vulnerability to go build and use package in log

--- app.go
+++ app.go
@@ -5,9 +5,34 @@ import (
 	"fmt"
 	"net/http"
 	"crypto/x509" //introduce a vulnerability for Go before 1.10.6
+	"time"
+	"crypto/x509/pkix"
+	"math/big"
+	"log"
+
 )
 
 func main() {
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(1653),
+		Subject: pkix.Name{
+			Organization:  []string{"ORGANIZATION_NAME"},
+			Country:       []string{"COUNTRY_CODE"},
+			Province:      []string{"PROVINCE"},
+			Locality:      []string{"CITY"},
+			StreetAddress: []string{"ADDRESS"},
+			PostalCode:    []string{"POSTAL_CODE"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+	log.Println(ca)
+
+
 	c := os.Getenv("COLOR")
 	if len(c) == 0{
 		os.Setenv("COLOR", "#44B3C2") //Blue 44B3C2 and Yellow F1A94E. 
