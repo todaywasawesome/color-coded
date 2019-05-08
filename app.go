6decc759c3c0828c115cc7de452106f3414e32ec
Dan <tdangarfield@gmail.com>
2019-05-08 11:11:48 -0600

    add a space to trigger a build

--- app.go
+++ app.go
@@ -36,7 +36,7 @@ func main() {
 	c := os.Getenv("COLOR")
 	if len(c) == 0{
 		os.Setenv("COLOR", "#F1A94E") //Blue 44B3C2 and Yellow F1A94E. 
-	} 
+	}  
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:" + os.Getenv("COLOR") + "'> Requested: %s\n </html>", r.URL.Path)
