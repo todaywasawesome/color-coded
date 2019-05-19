abb8b467836e1567787bd5dff4d3783273d498f2
Dan <tdangarfield@gmail.com>
2019-05-19 09:24:35 +0200

    go blue

--- app.go
+++ app.go
@@ -1,41 +1,8 @@
 package main
  
-import (
-	"os"
-	"fmt"
-	"net/http"
-	"crypto/x509" //introduce a vulnerability for Go before 1.10.6
-	"time"
-	"crypto/x509/pkix"
-	"math/big"
-	"log"
-
-)
-
-func main() {
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(1653),
-		Subject: pkix.Name{
-			Organization:  []string{"ORGANIZATION_NAME"},
-			Country:       []string{"COUNTRY_CODE"},
-			Province:      []string{"PROVINCE"},
-			Locality:      []string{"CITY"},
-			StreetAddress: []string{"ADDRESS"},
-			PostalCode:    []string{"POSTAL_CODE"},
-		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(10, 0, 0),
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		BasicConstraintsValid: true,
-	}
-	log.Println(ca)
-
-
 	c := os.Getenv("COLOR")
 	if len(c) == 0{
-		os.Setenv("COLOR", "#F1A94E") //Blue 44B3C2 and Yellow F1A94E. 
+		os.Setenv("COLOR", "#44B3C2") //Blue 44B3C2 and Yellow F1A94E. 
 	}  
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
