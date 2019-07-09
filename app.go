c2d54928df8c26c3e659cd05b81b326b367b83ff
Dan <tdangarfield@gmail.com>
2019-07-09 15:44:22 -0600

    go blue

--- app.go
+++ app.go
@@ -10,7 +10,7 @@ func main() {
 
 	c := os.Getenv("COLOR")
 	if len(c) == 0{
-		os.Setenv("COLOR", "#F1A94E") //Blue 44B3C2 and Yellow F1A94E 
+		os.Setenv("COLOR", "#44B3C2") //Blue 44B3C2 and Yellow F1A94E 
 	}  
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
